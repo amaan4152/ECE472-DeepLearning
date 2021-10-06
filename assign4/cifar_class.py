24cf489272c56e65c3ca640745b7c01683194742
Programmer-00X <63811852+Programmer-00X@users.noreply.github.com>
2021-10-06 19:00:13 -0400

    ResNet 50 model setup

--- assign4/cifar_class.py
+++ assign4/cifar_class.py
@@ -2,12 +2,13 @@ import tensorflow as tf
 import numpy as np
 from matplotlib import pyplot as plt
 from tensorflow.python.keras.callbacks import LearningRateScheduler
+from assign4.resnet import ResNet_50
 from darse import Parser
 from tensorflow.keras import models, regularizers
-from tensorflow.keras.layers import Dense,Conv2D,Dropout,Flatten, MaxPooling2D, BatchNormalization
+from tensorflow.keras.layers import Dense,Conv2D,Dropout,Flatten, MaxPooling2D, BatchNormalization, Activation, Add
 from os import getpid
 
-BATCH_SIZE = 128
+BATCH_SIZE = 64
 EPOCHS = 40
 
 dataset = {
@@ -42,56 +43,49 @@ def lr_sched(epoch, lr):
             return lr * tf.math.exp(-0.1)
     return lr
 
+# https://www.analyticsvidhya.com/blog/2021/08/how-to-code-your-resnet-from-scratch-in-tensorflow/
+def VGG_blk(input, filter_depth):
+	out = Conv2D(filter_depth,
+				 kernel_size=(3,3),
+				 kernel_initializer='he_normal',
+				 padding='same')(input)
+
+	out = BatchNormalization(axis=3)(out)
+	out = Activation('elu')(out)
+	out = Conv2D(filter_depth,
+				 kernel_size=(3,3),
+				 kernel_initializer='he_normal',
+				 padding='same')(out)
+
+	out = BatchNormalization(axis=3)(out)
+	return out
+
+def ident_blk(input, filter_depth):
+	ff_input = input
+	out = VGG_blk(input, filter_depth)
+	out = Add()([out, ff_input])
+	out = Activation('elu')(out)
+	return out
+
+def conv_blk(input, filter_depth):
+	ff_input = input
+	out = VGG_blk(input, filter_depth)
+	ff_out = Conv2D(filter_depth,
+					kernel_size=(1,1),
+					kernel_initializer='he_normal',
+					strides=(2,2))(ff_input)
+
+	out = Add()([out, ff_out])
+	out = Activation('elu')(out)
+	return out
+
 def main():
 	cifar_parser = Parser(dataset, 'CIFAR')
 	train, test = cifar_parser.parse()
 	train_data, train_labels = train
 	test_data, test_labels = test
 	# model
-	model = models.Sequential()
-	model.add(Conv2D(64,
-					kernel_size=(2, 2),
-					kernel_initializer='he_normal',
-					kernel_regularizer=regularizers.l2(l2=0.01),
-					activation=tf.nn.elu,
-					input_shape=(train_data.shape[1], train_data.shape[2], 3)))
-	model.add(Conv2D(64,
-					kernel_size=(2, 2),
-					kernel_initializer='he_normal',
-					kernel_regularizer=regularizers.l2(l2=0.01),
-					activation=tf.nn.elu))
-	model.add(MaxPooling2D(strides=(2,2)))
-	model.add(Conv2D(256,
-					kernel_size=(2, 2),
-					kernel_initializer='he_normal',
-					kernel_regularizer=regularizers.l2(l2=0.01),
-					activation=tf.nn.elu))
-	model.add(Conv2D(256,
-					kernel_size=(2, 2),
-					kernel_initializer='he_normal',
-					kernel_regularizer=regularizers.l2(l2=0.01),
-					activation=tf.nn.elu))
-	model.add(Conv2D(256,
-					kernel_size=(2, 2),
-					kernel_initializer='he_normal',
-					kernel_regularizer=regularizers.l2(l2=0.01),
-					activation=tf.nn.elu))
-	model.add(Conv2D(256,
-					kernel_size=(2, 2),
-					kernel_initializer='he_normal',
-					kernel_regularizer=regularizers.l2(l2=0.01),
-					activation=tf.nn.elu))     
-	model.add(MaxPooling2D(strides=(2, 2)))
-	model.add(Flatten())
-	model.add(Dropout(0.5))
-	model.add(Dense(4096, 
-							activation=tf.nn.leaky_relu,                      
-							kernel_regularizer=regularizers.l2(l2=0.0005)))
-	model.add(Dropout(0.25))
-	model.add(BatchNormalization())
-	model.add(Dense(10, 
-							activation='softmax',                      
-							kernel_regularizer=regularizers.l2(l2=0.0005)))
+	model = ResNet_50((test_data[1], test_data[2], 3))
 	model.summary()
 	# model compile
 	model.compile(
