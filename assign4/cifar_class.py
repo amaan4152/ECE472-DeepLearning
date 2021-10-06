07dc82cd4eaca44642dd106372dfc07a02c38ad1
amaan4152 <amaan.rahman.usa@gmail.com>
2021-10-06 19:08:02 +0000

    [EXP2]

--- assign4/cifar_class.py
+++ assign4/cifar_class.py
@@ -21,6 +21,7 @@ dataset = {
 
 # https://machinelearningmastery.com/how-to-develop-a-cnn-from-scratch-for-cifar-10-photo-classification/
 def plot_diagnostics(history):
+<<<<<<< Updated upstream
 	# plot loss
 	plt.subplot(211)
 	plt.title('Cross Entropy Loss')
@@ -35,82 +36,98 @@ def plot_diagnostics(history):
 	# save plot to file
 	plt.savefig('cifar10_plot_' + str(getpid()) + '.png')
 	plt.close()
+=======
+    # plot loss
+    plt.subplot(211)
+    plt.title('Cross Entropy Loss')
+    plt.plot(history.history['loss'], color='blue', label='train')
+    plt.plot(history.history['val_loss'], color='orange', label='validation')
+    # plot accuracy
+    plt.subplot(212)
+    plt.title('Classification Accuracy')
+    plt.plot(history.history['accuracy'], color='blue', label='train')
+    plt.plot(history.history['val_accuracy'], color='orange', label='validation')
+    plt.legend()
+    # save plot to file
+    plt.savefig('cifar10_plot_' + str(getpid()) + '.png')
+    plt.close()
+>>>>>>> Stashed changes
 
 # https://www.tensorflow.org/api_docs/python/tf/keras/callbacks/LearningRateScheduler
 def lr_sched(epoch, lr):
-	if epoch >= EPOCHS/2: 
-		return lr * tf.math.exp(-0.1)
-	return lr
+    if epoch >= EPOCHS/2: 
+            return lr * tf.math.exp(-0.1)
+    return lr
 
 def main():
-	cifar_parser = Parser(dataset, 'CIFAR')
-	train, test = cifar_parser.parse()
-	train_data, train_labels = train
-	test_data, test_labels = test
-	# model
-	model = models.Sequential()
-	model.add(Conv2D(64,
-			kernel_size=(2, 2),
-			kernel_initializer='he_normal',
-			kernel_regularizer=regularizers.l2(l2=0.01),
-			activation=tf.nn.elu,
-			input_shape=(train_data.shape[1], train_data.shape[2], 3)))
-	model.add(Conv2D(64,
-			kernel_size=(2, 2),
-			kernel_initializer='he_normal',
-			kernel_regularizer=regularizers.l2(l2=0.01),
-			activation=tf.nn.elu))
-	model.add(MaxPooling2D(strides=(2,2)))
-	model.add(Conv2D(256,
-			kernel_size=(2, 2),
-			kernel_initializer='he_normal',
-			kernel_regularizer=regularizers.l2(l2=0.01),
-			activation=tf.nn.elu))
-	model.add(Conv2D(256,
-			kernel_size=(2, 2),
-			kernel_initializer='he_normal',
-			kernel_regularizer=regularizers.l2(l2=0.01),
-			activation=tf.nn.elu))
-	model.add(Conv2D(256,
-			kernel_size=(2, 2),
-			kernel_initializer='he_normal',
-			kernel_regularizer=regularizers.l2(l2=0.01),
-			activation=tf.nn.elu))
-	model.add(Conv2D(256,
-			kernel_size=(2, 2),
-			kernel_initializer='he_normal',
-			kernel_regularizer=regularizers.l2(l2=0.01),
-			activation=tf.nn.elu))     
-	model.add(MaxPooling2D(strides=(2, 2)))
-	model.add(Flatten())
-	model.add(Dropout(0.5))
-	model.add(Dense(2048, 
-				activation=tf.nn.leaky_relu,                      
-				kernel_regularizer=regularizers.l2(l2=0.0005)))
-	model.add(Dropout(0.5))
-	model.add(Dense(1024, 
-				activation=tf.nn.leaky_relu,                      
-				kernel_regularizer=regularizers.l2(l2=0.0005)))
-	model.add(Dense(10, 
-				activation='softmax',                      
-				kernel_regularizer=regularizers.l2(l2=0.0005)))
-	model.summary()
-	# model compile
-	model.compile(
-		optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
-	)
-	callback = LearningRateScheduler(lr_sched)
-	history = model.fit(
-		x=train_data,
-		y=train_labels,
-		batch_size=BATCH_SIZE,
-		epochs=EPOCHS,
-		steps_per_epoch=np.math.ceil(0.8 * train_data.shape[0] / BATCH_SIZE),
-		validation_split=0.2,
-		callbacks=[callback]
-	)
-	model.evaluate(x=test_data, y=test_labels)
-	plot_diagnostics(history)
+    cifar_parser = Parser(dataset, 'CIFAR')
+    train, test = cifar_parser.parse()
+    train_data, train_labels = train
+    test_data, test_labels = test
+    # model
+    model = models.Sequential()
+    model.add(Conv2D(64,
+                    kernel_size=(2, 2),
+                    kernel_initializer='he_normal',
+                    kernel_regularizer=regularizers.l2(l2=0.01),
+                    activation=tf.nn.elu,
+                    input_shape=(train_data.shape[1], train_data.shape[2], 3)))
+    model.add(Conv2D(64,
+                    kernel_size=(2, 2),
+                    kernel_initializer='he_normal',
+                    kernel_regularizer=regularizers.l2(l2=0.01),
+                    activation=tf.nn.elu))
+    model.add(MaxPooling2D(strides=(2,2)))
+    model.add(Conv2D(256,
+                    kernel_size=(2, 2),
+                    kernel_initializer='he_normal',
+                    kernel_regularizer=regularizers.l2(l2=0.01),
+                    activation=tf.nn.elu))
+    model.add(Conv2D(256,
+                    kernel_size=(2, 2),
+                    kernel_initializer='he_normal',
+                    kernel_regularizer=regularizers.l2(l2=0.01),
+                    activation=tf.nn.elu))
+    model.add(Conv2D(256,
+                    kernel_size=(2, 2),
+                    kernel_initializer='he_normal',
+                    kernel_regularizer=regularizers.l2(l2=0.01),
+                    activation=tf.nn.elu))
+    model.add(Conv2D(256,
+                    kernel_size=(2, 2),
+                    kernel_initializer='he_normal',
+                    kernel_regularizer=regularizers.l2(l2=0.01),
+                    activation=tf.nn.elu))     
+    model.add(MaxPooling2D(strides=(2, 2)))
+    model.add(Flatten())
+    model.add(Dropout(0.5))
+    model.add(Dense(2048, 
+                            activation=tf.nn.leaky_relu,                      
+                            kernel_regularizer=regularizers.l2(l2=0.0005)))
+    model.add(Dropout(0.5))
+    model.add(Dense(1024, 
+                            activation=tf.nn.leaky_relu,                      
+                            kernel_regularizer=regularizers.l2(l2=0.0005)))
+    model.add(Dense(10, 
+                            activation='softmax',                      
+                            kernel_regularizer=regularizers.l2(l2=0.0005)))
+    model.summary()
+    # model compile
+    model.compile(
+            optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
+    )
+    callback = LearningRateScheduler(lr_sched)
+    history = model.fit(
+            x=train_data,
+            y=train_labels,
+            batch_size=BATCH_SIZE,
+            epochs=EPOCHS,
+            steps_per_epoch=np.math.ceil(0.8 * train_data.shape[0] / BATCH_SIZE),
+            validation_split=0.2,
+            callbacks=[callback]
+    )
+    model.evaluate(x=test_data, y=test_labels)
+    plot_diagnostics(history)
 
 if __name__ == '__main__':
     main()
