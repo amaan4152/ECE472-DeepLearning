5c0c67d3ca445903d0fdc1886e3f12ef69e8a43e
Programmer-00X <63811852+Programmer-00X@users.noreply.github.com>
2021-10-07 12:02:30 -0400

    rm padding

--- assign4/resnet.py
+++ assign4/resnet.py
@@ -11,16 +11,13 @@ def VGG_blk(input, filter_depth, s):
                 kernel_size=(3,3),
                 kernel_initializer='he_normal',
                 kernel_regularizer=regularizers.l2(l2=0.0001),
-                padding='same',
                 strides=s)(input)
-    out = ZeroPadding2D(padding=(8,8))(out)
     out = BatchNormalization(axis=3, momentum=0.9)(out)
     out = Activation('elu')(out)
     out = Conv2D(filter_depth,
                 kernel_size=(3,3),
                 kernel_initializer='he_normal',
-                kernel_regularizer=regularizers.l2(l2=0.0001),
-                padding='same')(out)
+                kernel_regularizer=regularizers.l2(l2=0.0001))(out)
 
     out = BatchNormalization(axis=3, momentum=0.9)(out)
     return out
