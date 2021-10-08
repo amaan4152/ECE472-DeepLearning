2bf4dfb49fdda4e729fe30d7894cc5d3b179fb05
Programmer-00X <63811852+Programmer-00X@users.noreply.github.com>
2021-10-08 12:54:44 -0400

    modified resnet

--- assign4/resnet.py
+++ assign4/resnet.py
@@ -6,13 +6,14 @@ from tensorflow.keras.layers import Dense, Conv2D, Dropout, Flatten, MaxPooling2
 from tensorflow.keras.layers.experimental.preprocessing import RandomCrop
 # https://www.analyticsvidhya.com/blog/2021/08/how-to-code-your-resnet-from-scratch-in-tensorflow/
 # https://www.cv-foundation.org/openaccess/content_cvpr_2016/papers/He_Deep_Residual_Learning_CVPR_2016_paper.pdf
-def VGG_blk(input, filter_depth, s):
+def basic_blk(input, filter_depth, s):
     out = Conv2D(filter_depth,
                 kernel_size=(3,3),
-                kernel_initializer='he_normal',
+                kernel_initializer='he_normal',  
                 kernel_regularizer=regularizers.l2(l2=0.0001),
                 padding='same',
                 strides=s)(input)
+                
     out = BatchNormalization(axis=3, momentum=0.9)(out)
     out = Activation('elu')(out)
     out = Conv2D(filter_depth,
@@ -26,15 +27,14 @@ def VGG_blk(input, filter_depth, s):
 
 def ident_blk(input, filter_depth):
     ff_input = input
-    out = VGG_blk(input, filter_depth, (1,1))
+    out = basic_blk(input, filter_depth, (1,1))
     out = Add()([out, ff_input])
-    out = Activation('elu')(out)
     return out
 
 
 def conv_blk(input, filter_depth, stride):
     ff_input = input
-    out = VGG_blk(input, filter_depth, stride)
+    out = basic_blk(input, filter_depth, stride)
     ff_out = Conv2D(filter_depth,
                     kernel_size=(3,3),
                     kernel_initializer='he_normal',
@@ -43,12 +43,11 @@ def conv_blk(input, filter_depth, stride):
                     padding='same')(ff_input)
     ff_out = BatchNormalization(axis=3)(ff_out)
     out = Add()([out, ff_out])
-    out = Activation('elu')(out)
     return out
 
 
-def res_blk(ID, x, filter_depth, blk_depth):
-    for i in range(blk_depth):
+def res_blk(ID, x, filter_depth, num_layers):
+    for i in range(num_layers):
         x = conv_blk(x, ID * filter_depth, (2,2))
     return x
 
@@ -68,6 +67,7 @@ def ResNet_N(in_shape, N):
 
     x = BatchNormalization(axis=3, momentum=0.9)(x)
     x = Activation('elu')(x)
+    
     layers = [2 * N] * 3
     for i in range(len(layers)):
         x = ident_blk(x, filter_depth)
@@ -76,7 +76,7 @@ def ResNet_N(in_shape, N):
     
     x = GlobalAveragePooling2D()(x)
     x = Flatten()(x)
-    x = Dense(1000, activation=tf.nn.leaky_relu, kernel_regularizer=regularizers.l2(l2=0.0001))(x)
+    x = Dropout(0.25)(x)
     x = Dense(10, activation='softmax')(x)
     model = Model(inputs=input, outputs=x, name=('ResNet-' + str(6*N+2)))
 
