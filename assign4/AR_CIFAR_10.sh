75527f46452898233e24f995868e366a6aff43b6
amaan4152 <amaan.rahman.usa@gmail.com>
2021-10-09 03:16:38 +0000

    DROPOUT EXPERIMENT 3.0

---
+++ assign4/AR_CIFAR_10.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+#SBATCH --job-name=JOB_NAME
+#SBATCH --output=/zooper2/amaan.rahman/ECE472-DeepLearning/assign4/output.txt
+#SBATCH --gres=gpu:2
+#SBATCH --ntasks=1
+#SBATCH --gres-flags=enforce-binding
+#SBATCH --nodes=1-1
+#SBATCH --mem=1gb
+
+python3 /zooper2/amaan.rahman/ECE472-DeepLearning/assign4/cifar_class.py
