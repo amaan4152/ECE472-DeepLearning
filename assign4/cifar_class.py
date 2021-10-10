1643d03436d5044c5961281948d5008a8fd04db5
Programmer-00X <63811852+Programmer-00X@users.noreply.github.com>
2021-10-09 23:36:43 -0400

    CIFAR100 train

--- assign4/cifar_class.py
+++ assign4/cifar_class.py
@@ -1,9 +1,6 @@
-import tensorflow as tf
-from tensorflow_addons.optimizers import CyclicalLearningRate
-import numpy as np
+from tensorflow.python.keras.callbacks import ReduceLROnPlateau
+from tensorflow.keras.optimizers import Adam
 from matplotlib import pyplot as plt
-from tensorflow.python.keras.callbacks import LearningRateScheduler, ReduceLROnPlateau
-from tensorflow.keras.optimizers import Adam, Nadam
 from resnet import ResNet_N
 from darse import Parser
 from os import getpid
@@ -11,13 +8,20 @@ from os import getpid
 BATCH_SIZE = 32
 EPOCHS = 200
 
-dataset = {
+# CIFAR_10
+dataset_10 = {
     "train-01": "/zooper2/amaan.rahman/ECE472-DeepLearning/datasets/CIFAR10_DATASET/pkl/data_batch_1",
     "train-02": "/zooper2/amaan.rahman/ECE472-DeepLearning/datasets/CIFAR10_DATASET/pkl/data_batch_2",
     "train-03": "/zooper2/amaan.rahman/ECE472-DeepLearning/datasets/CIFAR10_DATASET/pkl/data_batch_3",
     "train-04": "/zooper2/amaan.rahman/ECE472-DeepLearning/datasets/CIFAR10_DATASET/pkl/data_batch_4",
     "train-05": "/zooper2/amaan.rahman/ECE472-DeepLearning/datasets/CIFAR10_DATASET/pkl/data_batch_5",
     "test": "/zooper2/amaan.rahman/ECE472-DeepLearning/datasets/CIFAR10_DATASET/pkl/test_batch",
+}
+
+# CIFAR_100
+dataset_100 = {
+    "train": "/zooper2/amaan.rahman/ECE472-DeepLearning/datasets/CIFAR100_DATASET/pkl/train",
+    "test": "/zooper2/amaan.rahman/ECE472-DeepLearning/datasets/CIFAR100_DATASET/pkl/test",
 }
 
 # https://machinelearningmastery.com/how-to-develop-a-cnn-from-scratch-for-cifar-10-photo-classification/
@@ -38,31 +42,24 @@ def plot_diagnostics(history):
 	plt.savefig('/zooper2/amaan.rahman/ECE472-DeepLearning/assign4/DPE2_1_cifar10_plot_' + str(getpid()) + '.png')
 	plt.close()
 
-# https://www.tensorflow.org/api_docs/python/tf/keras/callbacks/LearningRateScheduler
-def lr_sched(epoch, lr):
-	if epoch == 1:
-		return lr * 10
-	if epoch >= 10: 
-		if epoch % 5 == 0:
-			return lr/10
-	return lr
 
 def main():
 	# dataset parse
-	cifar_parser = Parser(dataset, 'CIFAR')
+	cifar_parser = Parser(dataset_10, 'CIFAR')
 	train, test = cifar_parser.parse()
 	train_data, train_labels = train
 	test_data, test_labels = test
+	STEPS = 0.8 * train_data.shape[0] // BATCH_SIZE
 
 	# model init
-	model = ResNet_N((test_data.shape[1], test_data.shape[2], 3), N=4) # N = 4 => ResNet_18
+	model = ResNet_N(in_shape = (test_data.shape[1], test_data.shape[2], 3), 
+					 layers = [2, 2, 2, 2], 
+					 classes = 10) 
 	model.summary()
 
 	# model compile
 	# https://towardsdatascience.com/super-convergence-with-cyclical-learning-rates-in-tensorflow-c1932b858252
 	# https://arxiv.org/pdf/1506.01186.pdf
-	STEPS = 0.8 * train_data.shape[0] // BATCH_SIZE
-	#CLR = CyclicalLearningRate(initial_learning_rate=5e-4, maximal_learning_rate=1e-1, step_size=2*STEPS, scale_fn=lambda x: 1 / (2.0 ** (x - 1)))
 	model.compile(optimizer=Adam(), loss="sparse_categorical_crossentropy", metrics=["accuracy"])
 
 	# fit
@@ -77,7 +74,7 @@ def main():
 			validation_split=0.2
 	)
 
-	# eval
+	# evaluate
 	model.evaluate(x=test_data, y=test_labels)
 	plot_diagnostics(history)
 
--- assign4/resnet.py
+++ assign4/resnet.py
@@ -3,33 +3,44 @@ from tensorflow.keras import regularizers
 from tensorflow.keras.layers import Dense, Conv2D, Dropout, Flatten, BatchNormalization, Activation, Add, Input, ZeroPadding2D, GlobalAveragePooling2D
 from tensorflow.keras.layers.experimental.preprocessing import RandomCrop, RandomFlip
 from tensorflow.python.keras.layers.preprocessing.image_preprocessing import HORIZONTAL
+import numpy as np
+
+BOTTLENECK = True
+
 # https://www.analyticsvidhya.com/blog/2021/08/how-to-code-your-resnet-from-scratch-in-tensorflow/
 # https://www.cv-foundation.org/openaccess/content_cvpr_2016/papers/He_Deep_Residual_Learning_CVPR_2016_paper.pdf
-def basic_blk(input, k, filter_depth, s):
-    out = Conv2D(filter_depth,
-                kernel_size=k[0],
-                kernel_initializer='he_normal',  
-                kernel_regularizer=regularizers.l2(l2=0.00001),
-                padding='same',
-                strides=s)(input)
-                
-    out = BatchNormalization(axis=3, momentum=0.9)(out)
+def basic_blk(input, k, f, s):
+    out = BatchNormalization(axis=3, momentum=0.9)(input)
     out = Activation('elu')(out)
-    out = Conv2D(filter_depth,
-                kernel_size=k[1],
-                kernel_initializer='he_normal',
-                kernel_regularizer=regularizers.l2(l2=0.00001),
-                padding='same')(out)
+    out = Conv2D(filters = f,
+                kernel_size = k[0],
+                kernel_initializer = 'he_normal',  
+                kernel_regularizer = regularizers.l2(l2=0.00001),
+                padding = 'same',
+                strides = s)(out)
 
     out = BatchNormalization(axis=3, momentum=0.9)(out)
+    out = Activation('elu')(out)          
+    out = Conv2D(filters = f,
+                kernel_size = k[1],
+                kernel_initializer = 'he_normal',
+                kernel_regularizer = regularizers.l2(l2=0.00001),
+                padding = 'same')(out)
+
+    if BOTTLENECK:
+        out = Conv2D(filters = (4 * f),
+                    kernel_size = k[2],
+                    kernel_initializer = 'he_normal',
+                    kernel_regularizer = regularizers.l2(l2=0.00001),
+                    padding = 'same')(out)
+    
     return out
 
 def ident_blk(input, filter_depth):
     ff_input = input
     out = basic_blk(input, (3,3), filter_depth, (1,1))
     out = Add()([out, ff_input])
-    out = Activation('elu')(out)
-    out = Dropout(0.4)(out)
+    # out = Dropout(0.4)(out)
     return out
 
 
@@ -44,19 +55,21 @@ def conv_blk(input, filter_depth, stride):
                     padding='same')(ff_input)
     ff_out = BatchNormalization(axis=3)(ff_out)
     out = Add()([out, ff_out])
-    out = Activation('elu')(out)
-    out = Dropout(0.5)(out)
+    # out = Dropout(0.5)(out)
     return out
 
 
 def res_blk(x, filter_depth, num_layers):
-    x = conv_blk(x, filter_depth, (2,2))
-    for i in range(num_layers - 1):
+    if not BOTTLENECK:
+        num_layers -= 1
+        x = conv_blk(x, filter_depth, (2,2))
+
+    for i in range(num_layers):
         x = ident_blk(x, filter_depth)
     return x
 
 
-def ResNet_N(in_shape, N):
+def ResNet_N(in_shape, layers, classes):
     filter_depth = 64
     input = Input(in_shape)
 
@@ -74,8 +87,7 @@ def ResNet_N(in_shape, N):
 
     x = BatchNormalization(axis=3, momentum=0.9)(x)
     x = Activation('elu')(x)
-
-    layers = [2] * N
+    
     for i in range(layers[0]):
         x = ident_blk(x, filter_depth)
 
@@ -85,7 +97,7 @@ def ResNet_N(in_shape, N):
     x = GlobalAveragePooling2D()(x)
     x = Flatten()(x)
     x = Dropout(0.15)(x)
-    x = Dense(10, activation='softmax')(x)
-    model = Model(inputs=input, outputs=x, name=('ResNet-' + str(4*N + 2)))
+    x = Dense(classes, activation='softmax')(x)
+    model = Model(inputs=input, outputs=x, name=('ResNet-' + str(np.sum(layers) + 2)))
 
     return model
