3088c3a043b1f74f32d577497fb4dad71c45d3c5
Programmer-00X <63811852+Programmer-00X@users.noreply.github.com>
2021-10-10 02:38:52 -0400

    CIFAR 100: ResNet 50 EXP1 - BN & Activ postorder

--- assign4/resnet.py
+++ assign4/resnet.py
@@ -2,6 +2,7 @@ from tensorflow.keras.models import Model
 from tensorflow.keras import regularizers
 from tensorflow.keras.layers import Dense, Conv2D, Dropout, Flatten, BatchNormalization, Activation, Add, Input, ZeroPadding2D, GlobalAveragePooling2D
 from tensorflow.keras.layers.experimental.preprocessing import RandomCrop, RandomFlip
+from tensorflow.python.keras.layers.pooling import AveragePooling2D
 from tensorflow.python.keras.layers.preprocessing.image_preprocessing import HORIZONTAL
 import numpy as np
 
@@ -10,32 +11,32 @@ BOTTLENECK = True
 # https://www.analyticsvidhya.com/blog/2021/08/how-to-code-your-resnet-from-scratch-in-tensorflow/
 # https://www.cv-foundation.org/openaccess/content_cvpr_2016/papers/He_Deep_Residual_Learning_CVPR_2016_paper.pdf
 def basic_blk(input, k, f, s):
-    out = BatchNormalization(axis=3, momentum=0.9)(input)
-    out = Activation('elu')(out)
-    #out = Dropout(0.5)(out)
+    out = Dropout(0.5)(input)
     out = Conv2D(filters = f,
                 kernel_size = k[0],
                 kernel_initializer = 'he_normal',  
                 kernel_regularizer = regularizers.l2(l2=0.00001),
                 padding = 'valid',
                 strides = s)(out)
-
     out = BatchNormalization(axis=3, momentum=0.9)(out)
-    out = Activation('elu')(out)          
+    out = Activation('elu')(out) 
+
     out = Conv2D(filters = f,
                 kernel_size = k[1],
                 kernel_initializer = 'he_normal',
                 kernel_regularizer = regularizers.l2(l2=0.00001),
                 padding = 'same')(out)
+    out = BatchNormalization(axis=3, momentum=0.9)(out)
+    out = Activation('elu')(out) 
 
     if BOTTLENECK:
-        out = BatchNormalization(axis=3, momentum=0.9)(out)
-        out = Activation('elu')(out) 
         out = Conv2D(filters = (4 * f),
                     kernel_size = k[2],
                     kernel_initializer = 'he_normal',
                     kernel_regularizer = regularizers.l2(l2=0.00001),
                     padding = 'valid')(out)
+        out = BatchNormalization(axis=3, momentum=0.9)(out)
+        out = Activation('elu')(out) 
     return out
 
 def ident_blk(input, filter_depth):
@@ -48,15 +49,15 @@ def ident_blk(input, filter_depth):
 def conv_blk(input, filter_depth, stride):
     ff_input = input
     out = basic_blk(input, (1,3,1), filter_depth, stride)
-    #out = Dropout(0.5)(out)
-    ff_input = BatchNormalization(axis=3)(ff_input)
-    ff_input = Activation('elu')(ff_input)
+    out = Dropout(0.5)(out)
     ff_input = Conv2D(4 * filter_depth,
                     kernel_size=(1,1),
                     kernel_initializer='he_normal',
                     kernel_regularizer=regularizers.l2(l2=0.00001),
                     strides=stride,
                     padding='valid')(ff_input)
+    ff_input = BatchNormalization(axis=3)(ff_input)
+    ff_input = Activation('elu')(ff_input)
     out = Add()([out, ff_input])
     return out
 
@@ -90,10 +91,10 @@ def ResNet_N(in_shape, layers, classes):
     for i in range(len(layers)):
         x = res_blk(x, (i + 1)*filter_depth, layers[i])
 
-    x = GlobalAveragePooling2D()(x)
+    x = AveragePooling2D(padding='same')(x)
     x = Flatten()(x)
     x = Dropout(0.3)(x)
-    x = Dense(classes, activation='softmax')(x)
+    x = Dense(classes, activation='softmax', kernel_initializer='he_normal')(x)
     model = Model(inputs=input, outputs=x, name=('ResNet-' + str(3*np.sum(layers) + 2)))
 
     return model
