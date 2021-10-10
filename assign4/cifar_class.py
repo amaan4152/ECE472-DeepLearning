73896fb36186766ee7965449904c04f443caf999
Programmer-00X <63811852+Programmer-00X@users.noreply.github.com>
2021-10-10 14:02:32 -0400

    CIFAR 100: ResNet 50 EXP2 - with dropouts

--- assign4/cifar_class.py
+++ assign4/cifar_class.py
@@ -11,7 +11,7 @@ import numpy as np
 
 CIFAR_TYPE = 100
 BATCH_SIZE = 128
-EPOCHS = 500
+EPOCHS = 200
 
 # CIFAR_10
 dataset_10 = {
@@ -64,7 +64,7 @@ def main():
 	STEPS = 0.8 * train_data.shape[0] // BATCH_SIZE
 	# model init
 	model = ResNet_N(in_shape = (test_data.shape[1], test_data.shape[2], 3), 
-					 layers = [3, 4, 23, 3], 
+					 layers = [3, 4, 6, 3], 
 					 classes = 100) 
 	model.summary()
 
