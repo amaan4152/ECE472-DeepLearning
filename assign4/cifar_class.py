097f651a08aa6c7fd475b685be14197e9828cf1b
Programmer-00X <63811852+Programmer-00X@users.noreply.github.com>
2021-10-10 23:16:53 -0400

    CIFAR 100: ResNet 18 - fixed SSS

--- assign4/cifar_class.py
+++ assign4/cifar_class.py
@@ -58,10 +58,11 @@ def gen_data(cifar_type):
 	test_labels = to_categorical(test_labels)
 
 	# shuffle data
-	sss = StratifiedShuffleSplit(n_splits=1, random_state=42)
+	sss = StratifiedShuffleSplit(n_splits=2, random_state=42)
 	train_ind = sss.split(train_data, train_labels)
-	train_ind = list(train_ind)
-	return train_data[train_ind[0][1]], train_labels[train_ind[0][1]], test_data, test_labels
+	for train_ind in sss.split(train_data, train_labels):
+		train_x, train_y = train_data[train_ind], train_labels[train_ind]
+	return train_x, train_y, test_data, test_labels
 
 
 def main():
