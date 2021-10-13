1fb8dc78883ebd6b4601e7a7fae33be1b132c00c
Programmer-00X <63811852+Programmer-00X@users.noreply.github.com>
2021-10-12 23:37:30 -0400

    ag_class.py

---
+++ assign5/ag_class.py
@@ -0,0 +1,50 @@
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.python.keras.utils.np_utils import to_categorical
+from tensorflow.keras.layers import Input, Embedding, Conv1D, MaxPooling1D, Flatten, Dense
+import numpy as np
+import re
+
+from parser import CLI_Parser
+from parser import Parser
+
+# takes in pandas dataframe of text data
+# https://medium.com/@saitejaponugoti/nlp-natural-language-processing-with-tensorflow-b2751aa8c460
+# https://machinelearningmastery.com/develop-word-embedding-model-predicting-movie-review-sentiment/
+def text_processing(train, test):
+    train_data, train_labels = train[-2::-1]
+    test_data, test_labels = test[-2::-1]
+    train_labels = to_categorical(train_labels)
+    test_labels = to_categorical(test_labels)
+    train_data = [re.sub(r'[^a-z\d\s]', '', string.lower()) for string in train_data]
+    max_len = max(train_data, key=len)
+    print(train_data[0:3])
+    word_tokenizer = Tokenizer(oov_token='<OOV>')
+    word_tokenizer.fit_on_texts(train_data)
+    train_data = word_tokenizer.texts_to_sequences(train_data)
+    train_data = pad_sequences(train_data, padding='post')
+    test_data = word_tokenizer.texts_to_sequences(test_data)
+    test_data = pad_sequences(test_data, padding='post')
+    train_data = np.array(train_data)
+    train_labels = np.array(train_labels)
+    test_data = np.array(test_data)
+    test_labels = np.array(test_labels)
+    train_size = len(word_tokenizer.word_index) + 1 # +1 for unknown words
+
+    return (train_data, train_labels, train_size, max_len, test_data, test_labels)
+
+
+
+def main():
+    args = CLI_Parser()()
+    parser = Parser(args.train, args.test)
+    train, test = parser._getAtrributes()
+    train_data, train_labels, train_size, max_len, test_data, test_labels = text_processing(train, test)
+
+
+if __name__ == "__main__":
+    main()
+
+
+    
+
