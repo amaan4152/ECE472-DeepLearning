2d622df575afd04c779bd7b7065242b22d1a96f5
Programmer-00X <63811852+Programmer-00X@users.noreply.github.com>
2021-10-13 23:37:48 -0400

    ASSIGNMENT 5: No encoder - kernel_size=8

--- assign5/ag_class.py
+++ assign5/ag_class.py
@@ -87,7 +87,7 @@ def main():
                           embedded_dims = EMBED_DIMS)(input)
     x = SpatialDropout1D(0.75)(x)
     x = Conv1D(filters = 512,
-               kernel_size = 5,
+               kernel_size = 8,
                activation = 'elu',
                kernel_initializer = 'he_normal', 
                kernel_regularizer = regularizers.l2(0.0001))(x)
