266d4e8d3a8a61718725a910b8b57f89e0f890cf
Programmer-00X <63811852+Programmer-00X@users.noreply.github.com>
2021-10-13 22:20:07 -0400

    ASSIGNMENT 5: Transformer encoder

---
+++ assign5/encoder.py
@@ -0,0 +1,63 @@
+from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras import regularizers, layers
+from tensorflow.keras.layers import (
+    MultiHeadAttention,
+    LayerNormalization,
+    Dense,
+    Conv1D,
+    Embedding,
+    Dropout,
+    Flatten,
+    BatchNormalization,
+    Activation,
+    Add,
+    Input,
+    GlobalAveragePooling1D,
+)
+from tensorflow import shape, range
+import numpy as np
+
+# https://keras.io/examples/nlp/text_classification_with_transformer/
+class Encoder(layers.Layer):
+    def __init__(self, num_heads, embedded_dims, feed_forward_dims: list):
+        super(Encoder, self).__init__()
+
+        eps = 1e-9
+        dropout_rate = 0.06667
+
+        self.Attention = MultiHeadAttention(num_heads = num_heads, key_dim = embedded_dims)
+        self.Norm1 = LayerNormalization(epsilon = eps)
+        self.Norm2 = LayerNormalization(epsilon = eps)
+        self.FC = Sequential(
+                    [   Dropout(dropout_rate * 1.5), 
+                        Dense(
+                        units = width,
+                        activation = 'elu',
+                        kernel_initializer = 'he_normal',
+                        kernel_regularizer = regularizers.l2(0.00001)
+                    ) for width in feed_forward_dims]
+        )
+
+    def __call__(self, input):
+        x = self.Attention(input, input)
+        x = self.Norm1(Add(input, x))
+        input = x
+        x = self.FC(input)
+        x = self.Norm2(Add(input, x))
+        
+        return x
+
+class PositionalEncoder(layers.Layer):
+    def __init__(self, vocab_size, max_len, embedded_dims):
+        super(PositionalEncoder, self).__init__()
+        self.word_embedding = Embedding(input_dim = vocab_size, output_dim = embedded_dims)
+        self.pos_embedding = Embedding(input_dim = max_len, output_dim = embedded_dims)
+
+    def __call__(self, x):  
+        max_len = shape(x)[-1]
+        pos = range(start = 0, limit = max_len, delta = 0)
+        pos = self.pos_embedding(pos)
+        x = self.word_embedding(x)
+        x = Add(x, pos)
+
+        return x
